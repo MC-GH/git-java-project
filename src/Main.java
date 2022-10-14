da9fffd40c6a9f8466f49032c871fb3ac557c04c
MC-GH <cornelismichael@hotmail.com>
2022-10-14 11:31:03 +0200

    Add loop to main branch

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Iteratie");
+        }
+        int i = 0;
+        while (i < 4) {
+            System.out.println("Nog een iteratie");
+            i++;
+        }
     }
 }
-
-Test
-
-Test
-
-        Test;
-
-//dit is een versie voor de testbranch. De main blijft onaangeroerd als ik deze push.
